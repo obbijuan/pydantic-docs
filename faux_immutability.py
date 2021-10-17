ea69556f29c5f58a1649f98defee2d87d2533902
obbijuan <juan.garces@outlook.com>
2021-10-17 19:58:37 -0300

    Faux immutability

---
+++ faux_immutability.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+
+"""
+Models can be configured to be immutable via allow_mutation = False
+"""
+class FooBarModel(BaseModel):
+    a: str
+    b: dict
+
+    class Config:
+        allow_mutation = False
+
+foobar = FooBarModel(a='hello', b={'apple': 'pear'})
+
+
+try:
+    foobar.a = 'different'
+except TypeError as e:
+    print(e)
+    # "FooBarModel" is immutable and does not support item assignment
+
+
+print(foobar.a)
+# hello
+print(foobar.b)
+# {'apple': 'pear'}
+foobar.b['apple'] = 'grape'
+print(foobar.b)
+# {'apple': 'grape'}
